4a10e35fe8e51ea602f37e66c09215084596e401
aseniuk <aseniuk@ualberta.ca>
2017-10-01 01:50:26 -0600

    Added second activity: working on passing info between main and secondary activity

--- app/src/main/java/com/example/aseniuk/aseniuk_countbook/MainActivity.java
+++ app/src/main/java/com/example/aseniuk/aseniuk_countbook/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.aseniuk.aseniuk_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+//
+//
+//    private TextView counterTextView;
+//    private Button AddCounterButton;
+//    private Button ResetButton;
+//
+//    private int counter = 0;
 
-    @Override
+//    private View.OnClickListener clickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            switch (view.getId()) {
+//                case R.id.AddCounterButton:
+//                    addCounterButtonClicked();
+//                    break;
+//                case R.id.ResetButton :
+//                    resetButtonClicked();
+//                    break;
+//
+//            }
+//        }
+//    };
+
+    public void addCounterButtonClicked(View v) {
+        if(v.getId() == R.id.AddCounterButton)
+        {
+            Intent i = new Intent(MainActivity.this, Display.class);
+            startActivity(i);
+        }
+    }
+
+    @Override //like the run method
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //counterTextView = (TextView) findViewById(R.id.counterTextView);
+//        AddCounterButton = (Button) findViewById(R.id.AddCounterButton);
+//        AddCounterButton.setOnClickListener(clickListener);
+//        ResetButton = (Button) findViewById(R.id.ResetButton);
+//        ResetButton.setOnClickListener(clickListener);
+
+        //initCounter();
     }
+
+
+//    public void addCounterButtonClicked(View view) {
+//        counter++;
+//        counterTextView.setText(String.valueOf(counter)); //integer to string
+//    }
+//
+//    public void resetButtonClicked(View view) {
+//        counter = 0;
+//        counterTextView.setText(String.valueOf(counter)); //integer to string
+//    }
 }
